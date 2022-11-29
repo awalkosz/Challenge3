95cf9470c7b225a71d092352f150cb98dbe86cf5
Awal <adrianwalkosz@gmail.com>
2022-11-28 22:46:41 -0700

    added prompts

--- script.js
+++ script.js
@@ -23,7 +23,17 @@ generateBtn.addEventListener("click", writePassword);
 
 // creating menu prompts for pop to select which types of chars the user wants
 
+function PasswordPrompts () {
+  var PasswordLengths = prompt ("Choose length of password");
+  while ((PasswordLengths < 8) || (PasswordLengths > 128)) {
+    PasswordLengths = prompt ("Password length must be between 8 and 128 Characters");
+  }
+  var ConfirmNums = confirm("Do you want to include numbers in your password?"); 
+  var ConfirmSymbls = confirm("Do you want to include symbols in your password?");
+  var ConfirmUprCase = confirm("Do you want to include upper case letters in your password?");
+  var ConfirmLwrCase = confirm("Do you want to include lower case letters in your password?");
 
+}
 
 // Fetching random index and rounds down to equal integer to the correct length of the array
 function CreateRandom(length) {
